1c80e0c142c0adcd71f11c346a9d8825eb81abae
David Griver <david.griver@bioeye.com>
2021-12-08 17:04:41 +0200

    add arrow

--- david.py
+++ david.py
@@ -1,13 +1,15 @@
 from brainflow import BrainFlowInputParams, BoardShim, BoardIds
 import time
 from psychopy import visual, core
+from psychopy.visual import ShapeStim
+
 
 win = visual.Window()
-msg = visual.TextStim(win, text=u"Hello Ido")
-
-msg.draw()
-win.flip()
-core.wait(1)
+RightArrowVert = [(-0.4, 0.05), (-0.4, -0.05), (-.2, -0.05), (-.2, -0.1), (0, 0), (-.2, 0.1), (-.2, 0.05)]
+arrow = ShapeStim(win, vertices=RightArrowVert, fillColor='darkred', size=.5, lineColor='red')
+arrow.draw()
+win.update()
+core.wait(10)
 win.close()
 
 
@@ -17,14 +19,12 @@ def create_board(id: int):
     board.prepare_session()
     return board
 
-def record(n_trials = 5):
+
+def record(n_trials=5):
     board = create_board(-1)
     board.start_stream()
     for i in range(n_trials):
-
         time.sleep(1)
     board.stop_stream()
     data = board.get_board_data()
     board.release_session()
-
-
