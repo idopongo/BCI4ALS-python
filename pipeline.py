1288a0e4e22d695ec7a724aec92909c13835b981
David Griver <david.griver@bioeye.com>
2021-12-15 22:12:49 +0200

    split main into recording and pipeline

---
+++ pipeline.py
@@ -0,0 +1,34 @@
+import mne
+from Marker import Marker
+from constants import *
+import os
+import json
+from features import get_features
+from classifier import create_classifier
+
+def main():
+    raw, params = load_recordings("David")
+    epochs, labels = get_epochs(raw, params["trial_duration"])
+    features = get_features(epochs.get_data())
+    classifier = create_classifier(features, labels)
+
+def get_epochs(raw, trial_duration):
+    events = mne.find_events(raw)
+    # TODO: add proper baseline
+    epochs = mne.Epochs(raw, events, Marker.all(), 0, trial_duration, picks="data", baseline=(0, 0))
+    labels = epochs.events[:, -1]
+    return epochs, labels
+
+
+def load_recordings(subj):
+    recs = os.listdir(RECORDINGS_DIR)
+    subj_recs = [rec for rec in recs if rec.split("_")[1] == subj]
+    raws = [mne.io.read_raw_fif(os.path.join(RECORDINGS_DIR, rec, 'raw.fif')) for rec in recs]
+    with open(os.path.join(RECORDINGS_DIR, subj_recs[0], 'params.json')) as file:
+        params = json.load(file)
+    all_raw = mne.io.concatenate_raws(raws)
+    return all_raw, params
+
+
+if __name__ == "__main__":
+    main()
---
+++ recording.py
@@ -0,0 +1,85 @@
+from brainflow import BrainFlowInputParams
+from psychopy import core, visual
+import numpy as np
+from time import sleep
+import os
+from datetime import datetime
+from pathlib import Path
+from Marker import Marker
+from constants import *
+import mne
+from preprocessing import preprocess
+from classifier import create_classifier
+import json
+
+def main():
+    subj = input("Enter Subject Name: ")
+    params = {
+        'trial_duration': 1,
+        'trials_per_stim': 1,
+        'trial_gap': 1,
+    }
+    raw = run_session(**params)
+    save_raw(subj, raw, params)
+
+def save_raw(subj, raw, params):
+    folder_path = create_session_folder(subj)
+    raw.save(os.path.join(folder_path, "raw.fif"))
+    with open(os.path.join(folder_path, "params.json"), 'w', encoding='utf-8') as f:
+        json.dump(params, f, ensure_ascii=False, indent=4)
+
+def create_session_folder(subj):
+    date_str = datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
+    folder_name = f'{date_str}_{subj}'
+    folder_path = os.path.join(RECORDINGS_DIR, folder_name)
+    Path(folder_path).mkdir(exist_ok=True)
+    return folder_path
+
+def run_session(trials_per_stim=3, trial_duration=1, trial_gap=1):
+    trial_stims = np.tile(Marker.all(), trials_per_stim)
+    np.random.shuffle(trial_stims)
+
+    # start recording
+    board = create_board()
+    board.start_stream()
+
+    # display trials
+    win = visual.Window(units="norm")
+    for stim in trial_stims:
+        sleep(trial_gap)
+        show_stimulus(win, stim)
+        board.insert_marker(stim)
+        sleep(trial_duration)
+        win.flip()  # hide stimulus
+
+    # stop recording
+    raw = convert_to_mne(board.get_board_data())
+    board.stop_stream()
+    board.release_session()
+
+    return raw
+
+
+def show_stimulus(win, stim):
+    visual.ImageStim(win=win, image=Marker(stim).image_path, units="norm", size=2).draw()
+    win.update()
+
+
+def create_board():
+    params = BrainFlowInputParams()
+    board = BoardShim(BOARD_ID, params)
+    board.prepare_session()
+    return board
+
+
+def convert_to_mne(recording):
+    recording[EEG_CHANNELS] = recording[EEG_CHANNELS] / 1e6  # BrainFlow returns uV, convert to V for MNE
+    data = recording[EEG_CHANNELS + [MARKER_CHANNEL]]
+    ch_types = ['eeg'] * len(EEG_CHANNELS) + ['stim']
+    ch_names = EEG_CHAN_NAMES + [EVENT_CHAN_NAME]
+    info = mne.create_info(ch_names=ch_names, sfreq=FS, ch_types=ch_types)
+    raw = mne.io.RawArray(data, info)
+    return raw
+
+if __name__ == "__main__":
+    main()
