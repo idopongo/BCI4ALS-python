a4dfae2eb246b57a3a4951d82e48d70383676a7c
avigail79 <87430049+avigail79@users.noreply.github.com>
2021-12-28 17:29:56 +0200

    add plots and saving

--- figures.py
+++ figures.py
@@ -2,20 +2,17 @@ import matplotlib.pyplot as plt
 import os
 from constants import *
 import mne
-from pathlib import Path:wq
-
-
-
+from pathlib import Path
 
 
 def psd_plots(rec_folder_name):
-    folder_path = os.path.join(RECORDINGS_DIR, rec_folder_name, 'raw.fif')
+    rec_folder_path = os.path.join(RECORDINGS_DIR, rec_folder_name)
+    folder_path = os.path.join(rec_folder_path, 'raw.fif')
     raw = mne.io.read_raw_fif(folder_path)
-    folder_path_fig = os.path.join(RECORDINGS_DIR, rec_folder_name, "figures")
-    raw.plot_psd(fmax=50)
+    folder_path_fig = os.path.join(rec_folder_path, "figures")
     Path(folder_path_fig).mkdir(exist_ok=True)
-    plt.savefig(folder_path_fig)  #dir + r'\psd_plot.png')
-    plt.close()
 
-
-psd_plots("2021-12-19--21-18-28_David")
+    fig_psd = mne.viz.plot_raw_psd(raw, fmax=HIGH_PASS, show=False)
+    fig_psd.savefig(os.path.join(folder_path_fig, "psd.png"))
+    fig_raw = mne.viz.plot_raw(raw)
+    fig_raw.savefig(os.path.join(folder_path_fig, "raw_data.png"))
