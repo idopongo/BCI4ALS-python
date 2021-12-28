0c221163c1978126ffffa18800021ee7669f92f4
David Griver <david.griver@bioeye.com>
2021-12-28 12:14:30 +0200

    remove temp ICA

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,6 @@
 from constants import NOTCH_FREQ, NOTCH_WIDTH, LOW_PASS, HIGH_PASS
-from mne.preprocessing import ICA
 
 def preprocess(raw):
     raw.notch_filter(NOTCH_FREQ, notch_widths=NOTCH_WIDTH)
     raw.filter(LOW_PASS, HIGH_PASS)
-    # ica = ICA(n_components=13)
-    # ica.fit(raw)
-    # ica.plot_components()
     return raw
