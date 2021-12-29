57c1f277aa9e9e42fc3e53c797531d2dc3a43f4f
avigail79 <87430049+avigail79@users.noreply.github.com>
2021-12-29 15:42:31 +0200

    change names

--- figures.py
+++ figures.py
@@ -1,18 +1,17 @@
-import matplotlib.pyplot as plt
 import os
 from constants import *
 import mne
 from pathlib import Path
 
 
-def psd_plots(rec_folder_name):
+def save_plots(rec_folder_name):
     rec_folder_path = os.path.join(RECORDINGS_DIR, rec_folder_name)
-    folder_path = os.path.join(rec_folder_path, 'raw.fif')
-    raw = mne.io.read_raw_fif(folder_path)
-    folder_path_fig = os.path.join(rec_folder_path, "figures")
-    Path(folder_path_fig).mkdir(exist_ok=True)
+    raw_path = os.path.join(rec_folder_path, 'raw.fif')
+    raw = mne.io.read_raw_fif(raw_path)
+    fig_path = os.path.join(rec_folder_path, "figures")
+    Path(fig_path).mkdir(exist_ok=True)
 
     fig_psd = mne.viz.plot_raw_psd(raw, fmax=HIGH_PASS, show=False)
-    fig_psd.savefig(os.path.join(folder_path_fig, "psd.png"))
-    fig_raw = mne.viz.plot_raw(raw)
-    fig_raw.savefig(os.path.join(folder_path_fig, "raw_data.png"))
+    fig_psd.savefig(os.path.join(fig_path, "psd.png"))
+    fig_raw = mne.viz.plot_raw(raw, show=False)
+    fig_raw.savefig(os.path.join(fig_path, "raw_data.png"))
