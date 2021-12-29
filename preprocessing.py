6c93757fe70d75150c5edb746ab33081a2f5eecf
avigail79 <87430049+avigail79@users.noreply.github.com>
2021-12-29 15:34:56 +0200

    remove ica

--- preprocessing.py
+++ preprocessing.py
@@ -4,7 +4,4 @@ from constants import NOTCH_FREQ, NOTCH_WIDTH, LOW_PASS, HIGH_PASS
 def preprocess(raw):
     raw.notch_filter(NOTCH_FREQ, notch_widths=NOTCH_WIDTH)
     raw.filter(LOW_PASS, HIGH_PASS)
-    # raw.preprocessing.ICA()
-    # https://mne.tools/stable/generated/mne.preprocessing.ICA.html?highlight=ica#mne.preprocessing.ICA
-    # todo: add ICA
     return raw
