66a2a2fa0d161ca8175011089aa788e727a4d66b
David Griver <david.griver@bioeye.com>
2022-02-24 19:54:54 +0200

    add changes

--- Workflows.py
+++ Workflows.py
@@ -1,14 +1,6 @@
-from recording import run_session, save_raw, load_rec_params
-import os
-from pipeline import create_and_fit_pipeline, evaluate_pipeline
-from figures import create_and_save_plots
-
-
-def record_data(rec_params):
-    raw = run_session(rec_params)
-    folder_path = save_raw(raw, rec_params)
-    create_and_save_plots(os.path.basename(folder_path))
-    return raw
+from recording import record_data, load_rec_params
+from pipeline import create_and_fit_pipeline, evaluate_pipeline, load_recordings, get_epochs, \
+    grid_search_pipeline_hyperparams, save_hyperparams
 
 
 def record_and_create_pipeline(rec_params):
@@ -20,11 +12,22 @@ def record_and_create_pipeline(rec_params):
 
 def record_create_pipeline_to_online(rec_params):
     pipeline = record_and_create_pipeline()
-    raw = run_session(rec_params, pipeline)
-    folder_path = save_raw(raw, rec_params)
-    create_and_save_plots(os.path.basename(folder_path))
+    record_data(rec_params, pipeline)
+
+
+def create_pipeline_for_subject(subject):
+    raw, rec_params = load_recordings(subject)
+    pipeline, epochs, labels = create_and_fit_pipeline(raw, rec_params)
+    evaluate_pipeline(pipeline, epochs, labels)
+
+
+def find_best_hyperparams_for_subject(subject):
+    raw, rec_params = load_recordings(subject)
+    epochs, labels = get_epochs(raw, rec_params['trial_duration'])
+    best_hyperparams = grid_search_pipeline_hyperparams(epochs, labels)
+    save_hyperparams(best_hyperparams, subject)
 
 
 if __name__ == "__main__":
-    rec_params = load_rec_params()
-    record_data(rec_params)
+    # find_best_hyperparams_for_subject("Haggai2")
+    create_pipeline_for_subject("David")
