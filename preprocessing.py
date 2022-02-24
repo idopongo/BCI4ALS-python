42df92ef4c31117e272d19b9f4e258e6f1520c6c
avigail79 <87430049+avigail79@users.noreply.github.com>
2022-02-24 17:18:21 +0200

    change thresh num

--- preprocessing.py
+++ preprocessing.py
@@ -60,7 +60,7 @@ def reject_epochs(epochs, labels):
             elif abs(curr_chan.max()) > rejected_max_val:
                 bad_chan.append(i)
 
-        if len(bad_chan) > 2
+        if len(bad_chan) > 2:
             bad_epoch.append(epoch_idx)
 
     return np.delete(epochs, bad_epoch, axis=0), np.delete(labels, bad_epoch, axis=0)
