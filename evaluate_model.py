fixed mistake in calculation and simpler code

evaluate_model.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def main():
-    model_subject_name = 'Haggai'
+    model_subject_name = 'Haggai2'
     data_subject_name = 'Haggai2'
 
     # model
@@ -20,7 +20,7 @@ def main():
     predictions = pipeline.predict(epochs)
 
     # statistics
-    conf_matrix = get_confusion_matrix(labels, predictions)
+    conf_matrix = confusion_matrix(labels, predictions)
     print('confusion matrix (row=label, column=prediction):')
     print(conf_matrix)
 
@@ -30,34 +30,25 @@ def main():
 
 
 def calculate_true_and_false_rates(conf_matrix):
-    num_classes = 3
-    rates = np.zeros((num_classes, 2))
+
+    rates = np.zeros((3, 2))
 
     # true positive
     for i in range(0, 3):
         true_label_list = conf_matrix[i]
         total_num_true = sum(true_label_list)
-        hits = true_label_list[i]
-        rates[i][0] = hits/total_num_true
+        num_hits = true_label_list[i]
+        rates[i][0] = num_hits/total_num_true
 
     # false positive
     for i in range(0, 3):
-        num_false_pos = 0
-        num_true_pos = 0
-        for j in range(0, 3):
-            num_pred = conf_matrix[i, j]
-            if i == j:
-                num_true_pos = num_pred
-            else:
-                num_false_pos = num_false_pos + num_pred
-        rates[i][1] = num_false_pos/(num_false_pos + num_true_pos)
+        column = conf_matrix[:, i]
+        num_hits = column[i]
+        total_num_predictions = sum(column)
+        num_false_pos = total_num_predictions - num_hits
+        rates[i][1] = num_false_pos/total_num_predictions
 
     return rates
-
-
-def get_confusion_matrix(labels,predictions):
-    conf = confusion_matrix(labels, predictions)
-    return conf
 
 
 if __name__ == "__main__":