da2eda3de4071b33b8f9a9f1f5c8b3ed0c8f1752
David Griver <david.griver@bioeye.com>
2022-02-26 19:34:52 +0200

    reorganize hyperprams

--- src/Workflows.py
+++ src/Workflows.py
@@ -2,7 +2,7 @@ from recording import record_data
 from pipeline import create_and_fit_pipeline, evaluate_pipeline, get_epochs, \
     grid_search_pipeline_hyperparams
 
-from data_utils import load_recordings, load_hyperparams, save_hyperparams
+from data_utils import load_recordings, load_hyperparams, save_hyperparams, load_rec_params
 
 
 def record_and_create_pipeline(rec_params):
@@ -17,20 +17,26 @@ def record_create_pipeline_to_online(rec_params):
     record_data(rec_params, pipeline)
 
 
-def create_pipeline_for_subject(subject):
+def create_pipeline_for_subject(subject, pipeline_type="spectral"):
+    """
+    Create pipline of type in: ["spectral", "csp"]
+    """
+    print(f'Creating pipeline for subject {subject}...')
     raw, rec_params = load_recordings(subject)
     hyperparams = load_hyperparams(subject)
-    pipeline, epochs, labels = create_and_fit_pipeline(raw, rec_params, hyperparams=hyperparams)
+    pipeline, epochs, labels = create_and_fit_pipeline(raw, rec_params, hyperparams=hyperparams,
+                                                       pipeline_type=pipeline_type)
     evaluate_pipeline(pipeline, epochs, labels)
     return pipeline
 
 
-def find_best_hyperparams_for_subject(subject):
+def find_best_hyperparams_for_subject(subject, pipeline_type="spectral"):
     raw, rec_params = load_recordings(subject)
     epochs, labels = get_epochs(raw, rec_params['trial_duration'], reject_bad=not rec_params['use_synthetic_board'])
-    best_hyperparams = grid_search_pipeline_hyperparams(epochs, labels)
+    best_hyperparams = grid_search_pipeline_hyperparams(epochs, labels, pipeline_type)
     save_hyperparams(best_hyperparams, subject)
 
 
 if __name__ == "__main__":
-    create_pipeline_for_subject("Ido5")
+    rec_params = load_rec_params()
+    create_pipeline_for_subject("David3", pipeline_type="csp")
--- src/pipeline.py
+++ src/pipeline.py
@@ -9,53 +9,68 @@ import numpy as np
 from sklearn.model_selection import GridSearchCV
 from src.data_utils import load_recordings
 
+mne.set_log_level('warning')
+
 DEFAULT_HYPERPARAMS = {
     "preprocessing__epoch_tmin": 0.1,
     "preprocessing__l_freq": 2,
     "preprocessing__h_freq": 24,
-    "feature_extraction__n_fft": 250,
+    "feature_extraction__n_fft": 125,
     "feature_extraction__n_per_seg": 120,
-    "feature_extraction__n_overlap": 0,
+    "feature_extraction__n_overlap": 0.3,
     "feature_extraction__freq_bands": [8, 12, 30],
+    "CSP__n_components": 8,
 }
 
 
 def evaluate_pipeline(pipeline, epochs, labels):
-    skf = RepeatedStratifiedKFold(n_splits=4, n_repeats=10)
+    n_splits = 3
+    n_repeats = 10
+    print(f'Evaluating pipeline performance ({n_splits} splits, {n_repeats} repeats)...')
+    skf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats)
     scores = cross_val_score(pipeline, epochs, labels, cv=skf)
     print(
-        f'features classifier accuracy: \n mean: {np.round(np.mean(scores), 2)} \n std: {np.round(np.std(scores), 3)}')
+        f'Accuracy: \n mean: {np.round(np.mean(scores), 2)} \n std: {np.round(np.std(scores), 3)}')
 
 
-def create_and_fit_pipeline(raw, recording_params, hyperparams=DEFAULT_HYPERPARAMS):
+def create_and_fit_pipeline(raw, recording_params, hyperparams=DEFAULT_HYPERPARAMS,
+                            pipeline_type="spectral"):
     # get data, epochs
     epochs, labels = get_epochs(raw, recording_params["trial_duration"],
                                 reject_bad=not recording_params['use_synthetic_board'])
     # create a pipeline from params
-    pipeline = create_csp_pipeline(hyperparams)
+    pipeline = create_pipeline(hyperparams, pipeline_type=pipeline_type)
     pipeline.fit(epochs, labels)
     return pipeline, epochs, labels
 
 
-def create_pipeline(hyperparams=DEFAULT_HYPERPARAMS):
+def create_pipeline(hyperparams=DEFAULT_HYPERPARAMS, pipeline_type="spectral"):
     lda = LinearDiscriminantAnalysis()
-    pipeline = Pipeline([('preprocessing', Preprocessor()), ('feature_extraction', FeatureExtractor()), ('lda', lda)])
-    hyperparams = {key: hyperparams[key] for key in hyperparams if key.split("__")[0] in pipeline.get_params().keys()}
-    pipeline.set_params(**hyperparams)
+    if pipeline_type == "spectral":
+        pipeline = Pipeline(
+            [('preprocessing', Preprocessor()), ('feature_extraction', FeatureExtractor()), ('lda', lda)])
+    elif pipeline_type == "csp":
+        pipeline = Pipeline([('preprocessing', Preprocessor()), ('CSP', mne.decoding.CSP()), ('lda', lda)])
+    else:
+        raise ValueError(f'Pipeline type {pipeline_type} is not supported')
+
+    print(f'Creating {pipeline_type} pipeline: {show_pipeline_steps(pipeline)}')
+    relevant_hyperparams = filter_hyperparams_for_pipeline(hyperparams, pipeline)
+    pipeline.set_params(**relevant_hyperparams)
     return pipeline
 
 
-def create_csp_pipeline(hyperparams=DEFAULT_HYPERPARAMS):
-    lda = LinearDiscriminantAnalysis()
-    csp = mne.decoding.CSP()
-    pipeline = Pipeline([('preprocessing', Preprocessor()), ('CSP', csp), ('lda', lda)])
-    hyperparams = {key: hyperparams[key] for key in hyperparams if key.split("__")[0] in pipeline.get_params().keys()}
-    pipeline.set_params(**hyperparams)
-    return pipeline
+def filter_hyperparams_for_pipeline(hyperparams, pipeline):
+    return {key: hyperparams[key] for key in hyperparams if
+            key.split("__")[0] in pipeline.get_params().keys()}
 
 
-def grid_search_pipeline_hyperparams(epochs, labels):
-    pipeline = create_csp_pipeline()
+def show_pipeline_steps(pipeline):
+    return " => ".join(list(pipeline.named_steps.keys()))
+
+
+def grid_search_pipeline_hyperparams(epochs, labels, pipeline_type):
+    pipeline = create_pipeline(pipeline_type=pipeline_type)
     gridsearch_params = {
         "preprocessing__epoch_tmin": [0.1, 1],
         "preprocessing__l_freq": [2, 5, 7],
@@ -64,25 +79,9 @@ def grid_search_pipeline_hyperparams(epochs, labels):
         "feature_extraction__n_per_seg": [120],
         "feature_extraction__n_overlap": [0],
         "feature_extraction__freq_bands": [[11, 30], [8, 12, 30]],
-    }
-    skf = RepeatedStratifiedKFold(n_splits=3, n_repeats=5)
-    mne.set_log_level(verbose="WARNING")
-    gs = GridSearchCV(pipeline, gridsearch_params, cv=skf, n_jobs=-1, verbose=10, error_score="raise")
-    mne.set_log_level(verbose="INFO")
-    gs.fit(epochs, labels)
-    print("Best parameter (CV score=%0.3f):" % gs.best_score_)
-    print(gs.best_params_)
-    return gs.best_params_
-
-
-def grid_search_csp_hyperparams(epochs, labels):
-    pipeline = create_pipeline()
-    gridsearch_params = {
-        "preprocessing__epoch_tmin": [0, 0.5, 1, 1.5],
-        "preprocessing__l_freq": [5, 7],
-        "preprocessing__h_freq": [27, 30],
         "CSP__n_components": [5, 6, 7, 8]
     }
+    gridsearch_params = filter_hyperparams_for_pipeline(gridsearch_params, pipeline)
     skf = RepeatedStratifiedKFold(n_splits=3, n_repeats=5)
     mne.set_log_level(verbose="WARNING")
     gs = GridSearchCV(pipeline, gridsearch_params, cv=skf, n_jobs=-1, verbose=10, error_score="raise")
@@ -101,6 +100,7 @@ def get_epochs(raw, trial_duration, markers=Marker.all(), reject_bad=False):
     # consistent with the epochs
     epochs_data = epochs.get_data()
     labels = epochs.events[:, -1]
+    print(f'Found {len(labels)} epochs')
 
     if reject_bad:
         epochs_data, labels = reject_epochs(epochs_data, labels)
